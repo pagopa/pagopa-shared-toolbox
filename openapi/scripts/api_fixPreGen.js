84c487fbe93b7983e41fcd2671ae230748624a29
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-02-13 14:44:36 +0100

    fix: resolved typo on OpenAPI handling scripts

--- openapi/scripts/api_fixPreGen.js
+++ openapi/scripts/api_fixPreGen.js
@@ -3,6 +3,6 @@ const regexReplace = require('regex-replace');
 regexReplace(
   /("format": *"uri",[\s]*"type": "string")/gi,
   '"$ref": "#/definitions/STRINGWrapper"',
-  'openApi/generated/swagger20.json',
+  'openapi/generated/swagger20.json',
   { fileContentsOnly: true }
 );
